a1cc676369103f0ccbd04fa19b5833dfa1742f82
Avinash Jain <jainavi17@fb.com>
2024-05-26 20:52:35 +0100

    dockerising the app

--- houston/settings.py
+++ houston/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,7 +93,7 @@ DATABASES = {
         "NAME": "houston",
         "USER": "root",
         "PASSWORD": "avinash1",
-        "HOST": "localhost",
+        "HOST": "db",
         "PORT": "3306",
     }
 }
